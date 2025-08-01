6f1ffa58c7b9d2a6334bf776424cbe2b622d1908
BAD997 <breinerabello2018@gmail.com>
2025-08-01 07:11:46 -0500

    Mejora estructural

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,8 +3,8 @@ export const Footer = () => {
   return (
     <footer className="text-center font-Poppins">
     <p className="font-extralight">
-      Challenge by <a className="font-semibold" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a className="font-semibold" href="https://github.com/BAD997" target="_blank">Breiner Abello</a>.
+      Challenge by <a className="font-semibold" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer' >Frontend Mentor</a>. 
+      Coded by <a className="font-semibold" href="https://github.com/BAD997" target="_blank" rel='noopener noreferrer' >Breiner Abello</a>.
     </p>
   </footer>
   )
